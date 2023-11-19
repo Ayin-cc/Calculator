c8b16c94a0113220d51c3b4c08b2cda95c363007
Ayin <2752655197@qq.com>
2023-11-19 13:35:50 +0800

    逻辑实现

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,11 +3,13 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputControl;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Calculator extends Application {
+    private boolean isInputDouble = false;
+    private boolean isInputed = false;
+
     @Override
     public void start(Stage primaryStage) {
         // 新建窗体
@@ -22,54 +24,125 @@ public class Calculator extends Application {
 
         // 显示框
         TextField tempDisplay = new TextField();
+        TextField optDisplay = new TextField();
         TextField display = new TextField();
         tempDisplay.setEditable(false);
-        tempDisplay.setFocusTraversable(false);
         tempDisplay.setStyle("-fx-text-fill: gray;");
+        optDisplay.setEditable(false);
+        optDisplay.setStyle("-fx-text-fill: gray;");
         display.setEditable(false);
-        grid.add(tempDisplay, 0, 0, 4, 1);
+        display.setText("0");
+        grid.add(tempDisplay, 0, 0, 3, 1);
+        grid.add(optDisplay, 3, 0, 1, 1);
         grid.add(display, 0, 1, 4, 1);
 
         // 添加按钮
         String[] buttonLabels = {
-                "7", "8", "9", "+",
-                "4", "5", "6", "-",
-                "1", "2", "3", "*",
-                "C", "0", "=", "/"
+                "%", "²√", "^", "+",
+                "7", "8", "9", "-",
+                "4", "5", "6", "×",
+                "1", "2", "3", "÷",
+                "C", "0", ".", "=",
         };
 
-        for (int i = 0; i < 16; i++) {
+        for (int i = 0; i < 20; i++) {
             Button button = new Button(buttonLabels[i]);
             button.setMinSize(50, 50);
-            button.setOnAction(event -> handleButtonClick(button.getText(), tempDisplay, display));
+            button.setOnAction(event -> handleButtonClick(button.getText(), tempDisplay, optDisplay, display));
             grid.add(button, i % 4, i / 4 + 2);
         }
 
         // 显示
-        Scene scene = new Scene(grid, 265, 350);
+        Scene scene = new Scene(grid, 265, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void handleButtonClick(String value, TextField tempDisplay, TextField display) {
+    // 按钮的 handler
+    private void handleButtonClick(String value, TextField tempDisplay, TextField optDisplay, TextField display) {
         if (value.equals("C")) {
+            // 归零
+            isInputDouble = false;
+            isInputed = false;
             tempDisplay.clear();
-            display.clear();
-        } else if (value.equals("=")) {
+            optDisplay.clear();
+            display.setText("0");
+        }
+        else if (value.equals("=")) {
+            // 显示运算结果
             try {
-                String result = evaluateExpression(display.getText());
+                String result = evaluateExpression(Double.parseDouble(tempDisplay.getText()), Double.parseDouble(display.getText()), optDisplay.getText());
                 display.setText(result);
             } catch (Exception e) {
                 display.setText("Error");
             }
-        } else {
+            isInputDouble = false;
+            isInputed = true;
+            tempDisplay.clear();
+            optDisplay.clear();
+        }
+        else if(value.equals(".")){
+            // 小数
+            if(isInputDouble){
+                return;
+            }
+            isInputDouble = true;
+            isInputed = true;
             display.appendText(value);
+        }
+        else if(value.equals("+") || value.equals("-") || value.equals("×") || value.equals("÷") || value.equals("%") || value.equals("^")){
+            // 运算符
+            tempDisplay.setText(display.getText());
+            optDisplay.setText(value);
+            display.setText("0");
+            isInputed = false;
+            isInputDouble = false;
+        }
+        else if(value.equals("²√")){
+            // 平方根
+            try {
+                double res = Double.parseDouble(display.getText());
+                res = Math.sqrt(res);
+                display.setText(Double.toString(res));
+            } catch (Exception e) {
+                display.setText("Error");
+            }
+            isInputed = true;
+        }
+        else{
+            // 数字
+            if(!isInputed && !isInputDouble){
+                if(value.equals("0")){
+                    return;
+                }
+                isInputed = true;
+                display.clear();
+                display.appendText(value);
+            }
+            else{
+                display.appendText(value);
+            }
         }
     }
 
-    private String evaluateExpression(String expression) {
-        // 在这里实现表达式求值的逻辑，这里只是一个简单示例，可以使用 ScriptEngine 或自己实现一个求值器
-        return "";
+    // 实现计算
+    private String evaluateExpression(double a, double b, String opt) {
+        switch (opt){
+            case "+":
+                return Double.toString(a + b);
+            case "-":
+                return Double.toString(a - b);
+            case "×":
+                return Double.toString(a * b);
+            case "÷":
+                return Double.toString(a / b);
+            case "%":
+                return Double.toString(a % b);
+            case "^":
+                return Double.toString(Math.pow(a, b));
+            default:
+                return "Error";
+        }
     }
 
     public static void main(String[] args) {
